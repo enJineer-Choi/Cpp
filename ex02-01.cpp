e27977598ac4dca4204deed26268ab0ecd486243
최석진 (Choi Seok Jin) <54811217+enJineer-Choi@users.noreply.github.com>
2023-09-19 15:26:15 +0900

    Add files via upload

---
+++ ex02-01.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int score;
+	bool decision;
+
+
+	cout << "Enter your score: ";
+	cin >> score;
+
+	cout.setf(ios::boolalpha);
+
+	cout << (score >= 60) << endl;
+
+	decision = (score >= 60);
+	cout << decision << endl;
+
+	cout << !(false || true) << endl;
+	cout << ((5 > 6) && (7 / 0) )<< endl;
+	cout << !(5 && 7) << (!(!6)) << endl;
+
+	return 0;
+}
