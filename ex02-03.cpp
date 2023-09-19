7048b12fcbf5eb1dc601be67ebc597b7e987b95d
최석진 (Choi Seok Jin) <54811217+enJineer-Choi@users.noreply.github.com>
2023-09-20 00:09:36 +0900

    Add files via upload

---
+++ ex02-03.cpp
@@ -0,0 +1,38 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int score;
+	char grade;
+
+	cout << "Enter your score: ";
+	cin >> score;
+
+	switch (score / 10)
+	{
+	case 9:
+		grade = 'A';
+		cout << "Your grade is " << grade << endl;
+		break;
+	case 8:
+		grade = 'B';
+		cout << "Your grade is " << grade << endl;
+		break;
+	case 7:
+		grade = 'C';
+		cout << "Your grade is " << grade << endl;
+		break;
+	case 6:
+		grade = 'D';
+		cout << "Your grade is " << grade << endl;
+		break;
+	default:
+		grade = 'F';
+		cout << "Your grade is " << grade << endl;
+		break;
+
+	}
+	return 0;
+}
