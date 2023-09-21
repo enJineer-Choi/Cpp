8d729c71b33a9f9eb8780d96ab90b66dff10b4b1
최석진 (Choi Seok Jin) <54811217+enJineer-Choi@users.noreply.github.com>
2023-09-22 00:58:48 +0900

    Add files via upload

---
+++ ex03-02.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+#include <cmath>
+#include <cstdlib>
+
+using namespace std;
+
+int main()
+{
+	srand(5); //실습2
+
+	for (int i = 0; i < 10; i++) //실습 1
+		cout << rand() << endl;
+
+	for (int i = 0; i < 10; i++) //실습 3
+		cout << rand()%11 << endl;
+
+	cout << "Max= " << RAND_MAX << endl; //실습4
+
+	for (int i = 0; i < 10; i++) //실습 5
+		cout << (RAND_MAX-rand())/static_cast<double>(RAND_MAX) << endl;
+
+	return 0;
+}
