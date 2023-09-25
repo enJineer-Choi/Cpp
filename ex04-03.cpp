2e64052987aa9c7431f2e4be945c566a2d66a9cf
최석진 (Choi Seok Jin) <54811217+enJineer-Choi@users.noreply.github.com>
2023-09-26 07:48:20 +0900

    Add files via upload

---
+++ ex04-03.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+
+using namespace std;
+
+void print_data(int x, int y);
+void swap_data(int& x, int& y);
+int main()
+{
+	int a, b;
+	cout << "Enter two numbers : ";
+	cin >> a >> b;
+
+	cout << "Before swap : " << a<<" "<< b<<endl;
+	
+	
+	swap_data(a,b);
+	print_data(a, b);
+	return 0;
+}
+
+
+void swap_data(int &x,int &y) {
+	int temp;
+
+	temp = x;
+	x = y;
+	y = temp;
+
+}
+
+void print_data(int x, int y) {
+	cout << "After swap : " << x << " " << y << endl;
+}
+
