3866e16a796fba741c93d21f1b2cf9bde3504910
최석진 (Choi Seok Jin) <54811217+enJineer-Choi@users.noreply.github.com>
2023-09-28 01:46:41 +0900

    Add files via upload

---
+++ ex04-04.cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>
+
+using namespace std;
+void get_data(int* x, int* y);
+void print_data(int x, int y);
+void swap_data(int& x, int& y);
+int main()
+{
+	int a, b;
+	
+	get_data(&a, &b);
+	swap_data(a,b);
+	print_data(a, b);
+	return 0;
+}
+
+void get_data(int *x, int *y) {
+	cout << "Enter two numbers : ";
+	//cin >> x >> y; 주소값이 변경되어서 들어가게됨
+	cin >> *x >> *y;
+	cout << "Before swap : " << *x << " " << *y << endl;
+}
+
+
+void swap_data(int &x,int &y) {
+	int temp;
+
+	temp = x;
+	x = y;
+	y = temp;
+
+}
+
+void print_data(int x, int y) {
+	cout << "After swap : " << x << " " << y << endl;
+}
+
