1ec145d0bf0235e960848c7ffc94cafd3251b063
최석진 (Choi Seok Jin) <54811217+enJineer-Choi@users.noreply.github.com>
2023-09-28 01:47:10 +0900

    Add files via upload

---
+++ ex04-05.cpp
@@ -0,0 +1,41 @@
+﻿#include <iostream>
+
+using namespace std;
+void get_data(int& x, int& y, double& z);
+double avg(int x, int y);
+double avg(int x, double y);
+double avg(int x, int y, double z);
+
+int main()
+{
+	int a, b;
+	double c;
+
+	get_data(a,b,c);
+
+	//함수 오버로딩
+	cout << "The mean of two number= " << avg(a,b) << endl;
+	cout << "The mean of two number= " << avg(a,c) << endl;
+	cout << "The mean of two number= " << avg(a,b,c) << endl;
+
+	return 0;
+}
+
+void get_data(int &x, int &y,double &z) {
+	cout << "Enter two numbers : ";
+	//cin >> x >> y; 주소값이 변경되어서 들어가게됨
+	cin >> x >>y>>z;
+	
+}
+
+double avg(int x, int y) {
+	return (x + y) / 2.0;
+}
+
+double avg(int x, double y) {
+	return (x + y) / 2.0;
+}
+
+double avg(int x, int y,double z) {
+	return (x + y + z) / 3.0;
+}
