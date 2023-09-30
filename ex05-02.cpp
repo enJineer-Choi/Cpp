0801567a066fc8d746de4e3b3b3af424f220e0ed
최석진 (Choi Seok Jin) <54811217+enJineer-Choi@users.noreply.github.com>
2023-09-30 22:36:14 +0900

    Add files via upload

---
+++ ex05-02.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+
+using namespace std;
+void print_difference(int max, int data);
+int main()
+{
+	int i, max, sc[5], x;
+
+	cout << "Enter 5 numbers: ";
+	for (i = 0; i < 5; i++)
+		cin >> sc[i];
+	max = sc[0];
+	for (i = 1; i < 5; i++)
+		if (sc[i] > max)
+			max = sc[i];
+	cout << "Max = " << max << endl;
+	cout << "Defferences= ";
+	//인자로소 각 배열 요소
+	for (i = 0; i < 5; i++)
+	{
+		print_difference(max, sc[i]);
+	}
+
+	return 0;
+}
+void print_difference(int max, int data)
+{
+	cout << (max - data) << " ";
+}
