63a8d110bf3752ff22b76d59b69f5d2f198b3dbe
최석진 (Choi Seok Jin) <54811217+enJineer-Choi@users.noreply.github.com>
2023-10-04 21:43:44 +0900

    Add files via upload

---
+++ ex05-04.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+using namespace std;
+//void find_max(int sc[][4], int max[], int row);
+void find_max(int sc[][4], int max[], int row, int column);
+int main()
+{
+	int sc[2][4] = { {3,7,6,2},{10,16,14,13} };
+	int max[2];
+	
+	/*find_max(sc,max,2);*/
+	find_max(sc, max, 2,4);
+
+	cout << "Max0= " << max[0] << endl;
+	cout << "Max1= " << max[1] << endl;
+	return 0;
+}
+
+//void find_max(int sc[][4], int max[], int row)//배열의 차원을 함수 선언하는 곳에서 명시해주어야 함
+void find_max(int sc[][4], int max[], int row,int column) //void find_max(int sc[][column], int max[], int row,int column) 으로 받으면 오류 발생
+{
+	for (int i = 0; i < row; i++) {
+		max[i] = sc[i][0];// max의 초기화
+		for (int j = 0; j < column; j++)
+			if (sc[i][j] > max[i]) max[i] = sc[i][j];
+	}
+}
