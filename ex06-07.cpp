1ea2570d330f97a9c3196ca623630bc3f829f91a
최석진 (Choi Seok Jin) <54811217+enJineer-Choi@users.noreply.github.com>
2023-10-09 00:54:25 +0900

    Add files via upload

---
+++ ex06-07.cpp
@@ -0,0 +1,22 @@
+﻿#include "Accnt.h"
+
+int main()
+{
+	Account p1, p2;
+	//입력
+	p1.input_Account();
+	p2.input_Account();
+
+	// 잔액 증감
+	p1.up_value();
+	p2.down_value();
+
+	int m = 2;
+	p2.multi_value(m);
+	
+	//출력
+	p1.output_Account();
+	p2.output_Account();
+	
+	return 0;
+}
