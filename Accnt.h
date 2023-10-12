9629d44addea7f154b3d0c525007acf3ab905f27
최석진 (Choi Seok Jin) <54811217+enJineer-Choi@users.noreply.github.com>
2023-10-12 12:48:44 +0900

    Add files via upload

---
+++ Accnt.h
@@ -0,0 +1,23 @@
+﻿#include <iostream> //헤더파일에는 클래스에 대한 정보만 작성.
+using namespace std;
+
+class Account{
+private:
+	int number; //계좌번호
+	char name[10];  //예금주
+	double value;  //잔액
+public:
+	//입력
+	void input_Account();
+
+	//잔액 증감
+	void up_value();
+	void down_value();
+	void multi_value(int m);
+
+	//출력
+	void output_Account();
+
+};
+
+
