279212957fb3ad91c7b4339abc71cddb530ff79f
최석진 (Choi Seok Jin) <54811217+enJineer-Choi@users.noreply.github.com>
2023-10-12 12:48:33 +0900

    Add files via upload

---
+++ Accnt.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream> 
+#include "Accnt.h"
+using namespace std;
+
+
+//입력
+void Account::input_Account() {//input_Account()함수는 Account클래스에 속한다는 의미 -> 클래스 정의문제
+	cout << "ID= ";
+	cin >> number;
+	cout << "Name= ";
+	cin >> name;
+	cout << "value= ";
+	cin >> value;
+}
+
+
+//출력
+void Account::output_Account() {
+	cout << "ID= " << number << endl;
+	cout << "Name= " << name << endl;
+	cout << "Value= " << value << endl;
+}
+
+//잔액 증감
+void Account::up_value() {
+	value += 100;
+}
+
+void Account::down_value() {
+	value -= 100;
+}
+
+void Account::multi_value(int m) {
+	value *= m;
+}
