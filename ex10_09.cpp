a05158e0729d8b0150adcbbaff43c249ed806168
최석진 (Choi Seok Jin) <54811217+enJineer-Choi@users.noreply.github.com>
2023-11-14 00:00:42 +0900

    Add files via upload

---
+++ ex10_09.cpp
@@ -0,0 +1,22 @@
+// 10.2 동적 배열 - 다차원 동적 배열: 배열의 배열.         
+#include <iostream> 
+using namespace std;
+
+int main()
+{
+    // 배열 크기 입력 및 할당 
+    cout << "Size of matrix(r, c)= ?";
+    int rw, cl;
+    cin >> rw >> cl;
+
+    // 배열의 생성
+    
+    // 배열 채우기
+
+    cout << "출력\n"; 
+    // 배열 출력 
+    
+    // 배열 반환    
+             
+    return 0;
+}
